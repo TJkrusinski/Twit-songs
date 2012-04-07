e71c360ba415e710e9aae01a9e4479e870c37fa6
TJ Krusinski <tjkrus@gmail.com>
2012-04-07 15:51:30 -0400

    removed database stuff

--- twitter.js
+++ twitter.js
@@ -1,8 +1,29 @@
-var mongo = require('mongojs').connect('twitter', ['looks'])
+var mongo = require('mongojs').connect('twitter', ['looks']),
+		email = require("mailer");
 
 var twitter = {
 	saveLook: function(array){
-		mongo.looks.update({counts:count}, {$push:{looks:array}}, {upsert:true}, function(){});
+	//	mongo.looks.update({counts:count}, {$push:{looks:array}}, {upsert:true}, function(){});
+	},
+	
+	sendEmail: function(array){
+		email.send({
+		    ssl: true,
+		    host : "smtp.gmail.com",
+		    port : 465,
+		    domain : "[127.0.0.1]",            
+		    to : "tjkrus@gmail.com",
+		    from : "tjkrus@gmail.com",
+		    subject : "twit songs search",
+		    reply_to:"tjkrus@gmail.com",
+		   	html: JSON.stringify(array),
+		    authentication : "login",
+		    username : 'tjkrus@gmail.com',
+		    password : 'ilovedogs1',
+		  },
+		  function(err, result){
+		    if(err){ console.log(err); }
+		  });
 	}
 }
 
