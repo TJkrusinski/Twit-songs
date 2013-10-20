ba25d9634961922bc4111828964a0a136a287375
TJ Krusinski <tjkrus@gmail.com>
2013-10-20 15:24:30 -0400

    refactor

--- app.js
+++ app.js
@@ -1,26 +1,17 @@
+'use strict';
+
 var express = require('express'),
-		app = express.createServer(),
-		io = require('socket.io').listen(app),
-		funcs = require('./twitter.js').twitter,
-		Twitter = require('twitter');
+	app = express(),
+	server = require('http').createServer(app),
+	io = require('socket.io').listen(server),
+	Twitter = require('twitter');
 
+app.set('views', __dirname+'/views');
+app.engine('jade', require('jade').__express);
+app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'jade');
 
-
-
-app.configure(function(){
-	app.set('views', __dirname + '/views');
-	app.set('view engine', 'jade');
-	app.set('view options', {layout: false});
-	app.use(express.logger('dev'));
-	  app.use(express.bodyParser());
-	  app.use(express.methodOverride());
-	  app.use(express.cookieParser());
-	  app.use(app.router);
-	  app.use(express.static(__dirname + '/public'));
-	  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));	
-});
-
-app.listen(3000);
+server.listen(3000);
 
 var twitInfo = {
 	consumer_key: 'Fi5w9fJxwsY1y1fO9MCAUQ',
@@ -29,51 +20,24 @@ var twitInfo = {
 	access_token_secret: '6cAeMe88CQF6g8D59RIbGHrEr8crYVXuxgxVfyhjU'
 };
 
-var twit = new Twitter(twitInfo);
-/*
-twit.stream('statuses/filter', {track:['#obama']}, function(stream) {
-	stream.on('data', function (data) {
-		console.log(data.text);
-	});
-});
-*/
-app.get('/twitter?', function (req, res) {
-	res.render('twitter', {
-		
-	});
+app.get('*', function (req, res) {
+	res.render('twitter');
 });
 
-app.get('/', function(req, res){
-	res.redirect('/twitter');
-});
+io.set('log level', 0);
 
 io.sockets.on('connection', function (socket) {
-	
-	var listen = function(socket){
-		socket.twitter.on('data', function(data){
-			socket.emit('status', data);
-		})
-	}
+	var twitter = new Twitter(twitInfo);
 	
 	socket.on('track', function(status){
-		funcs.saveLook(status);
-		socket.twitter = new Twitter(twitInfo);
-		socket.twitter.stream('statuses/filter', {track:status}, function(stream){
-			socket.twitter = stream;
-		})
-		listen(socket);
+		twitter.stream('statuses/filter', {track:status}, function(stream){
+			stream.on('data', function(data){
+				socket.emit('status', data);
+			});
+		});
 	});
 	
 	socket.on('stop', function(status){
-		socket.twitter.destroy();
-	})
-	
-	socket.on('disconnect', function(){
-		
+		twitter.destroy();
 	});
-	
 });
-
-
-
-
